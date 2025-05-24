deafe4342dcc38630e1d80c58c569d218789ebbb
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2025-05-24 18:53:17 +0800

    added artist model

--- apps/dashboards/models.py
+++ apps/dashboards/models.py
@@ -16,11 +16,18 @@ class AlbumType(models.TextChoices):
     SINGLE = 'Single', 'Single'
     MIXTAPE = 'Mixtape', 'Mixtape'
 
+class Artist(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Album(BaseModel):
     title = models.CharField(max_length=200)
     release_date = models.DateField()
     album_type = models.CharField(max_length=20, choices=AlbumType.choices)
     language = models.CharField(max_length=50)
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE, related_name='albums', null=True)
 
     def __str__(self):
         return self.title
